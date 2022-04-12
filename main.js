e045386ff995ced289d1083bf342fa07588af275
Jacob B. Sanders <60639231+Segmentational@users.noreply.github.com>
2022-04-11 22:56:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
 const Test = async () => {
     const duration = 5000;
     const $ = async () => await new Promise(async (resolve) => {
+        process.stdout.write("Awaiting ..." + "\n");
         await setTimeout[Object.getOwnPropertySymbols(setTimeout)[0]](duration);
 
         resolve(true);
     });
 
     await $();
+    
+    process.stdout.write("Complete :D" + "\n");
 };
 
 export default (async () => await Test())();
